4b6ca4d75b203547eb24c25f48a1b656ace9f599
Артём Погирейчик <bligertip@gmail.com>
2026-01-22 19:40:12 +0200

    feat: add record deletion script

---
+++ www/delete.php
@@ -0,0 +1,13 @@
+﻿<?php
+require_once "db.php";
+require_once "GymRegistration.php";
+
+$gym = new GymRegistration($pdo);
+
+if (isset($_POST["id"]) && is_numeric($_POST["id"])) {
+    $gym->delete($_POST["id"]);
+}
+
+header("Location: index.php");
+exit();
+?>
